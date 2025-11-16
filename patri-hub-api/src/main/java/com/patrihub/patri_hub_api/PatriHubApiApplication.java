7429eff4693710dc1d5711c23cc932cf2cf54f44
Breno Guimarães <devbreno4@gmail.com>
2025-11-16 10:20:55 -0300

    feat:api

---
+++ patri-hub-api/src/main/java/com/patrihub/patri_hub_api/PatriHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.patrihub.patri_hub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatriHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatriHubApiApplication.class, args);
+	}
+
+}
---
+++ patri-hub-api/src/test/java/com/patrihub/patri_hub_api/PatriHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrihub.patri_hub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatriHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
