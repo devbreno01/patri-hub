45cdc50b5693c1d6cf01a2d603a5d8d83b2c5616
Breno Guimarães <devbreno4@gmail.com>
2025-11-23 02:25:52 -0300

    iniciando assets

--- patri-hub-frontend/src/pages/Assets/Assets.tsx
+++ patri-hub-frontend/src/pages/Assets/Assets.tsx
@@ -1,7 +1,28 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
+ 
 
 export default function AssetsPage(){
     return (
-        <h1>teste</h1>
+       <div className="p-16 max-w-4xl flex flex-wrap justify-around ">
+            <div className="border rounded">
+                <Table>
+                    <TableHeader>
+                        <TableHead>Nome</TableHead>
+                        <TableHead>Descrição</TableHead>
+                        <TableHead>Valor</TableHead>
+                        <TableHead>Status</TableHead>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>teste</TableCell>
+                            <TableCell>teste2</TableCell>
+                            <TableCell>teste3</TableCell>
+                            <TableCell>teste4</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+       </div>
     )
 }
